5b0276d1df8be54c258efbec1521daa0eae9dfea
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-10-10 12:16:14 -0400

    Add psql. Expand Move data

--- StriveDb.cs
+++ StriveDb.cs
@@ -7,16 +7,28 @@ public class StriveDb : DbContext
     public StriveDb(DbContextOptions<StriveDb> options) : base(options) { }
     public DbSet<Character> Characters => Set<Character>();
     public DbSet<Move> Moves => Set<Move>();
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Database=ggstrive;Username=chris;");
 }
 
 public class Character
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
+    public int CharId { get; set; }
+    public string? CharName { get; set; }
 }
 
 public class Move
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
+    public int MoveId { get; set; }
+    public int CharId { get; set; }
+    public string? MoveName { get; set; }
+    public string? Input { get; set; }
+    public string? Damage { get; set; }
+    public string? Guard { get; set; }
+    public string? Startup { get; set; }
+    public string? Active { get; set; }
+    public string? Recovery { get; set; }
+    public string? Block { get; set; }
+    public string? Invulnerability { get; set; }
 }
