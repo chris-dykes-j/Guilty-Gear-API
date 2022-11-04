663e0344879e144d82d9f0059e2f7751fa98269b
Christopher Dykes <Christopher.Dykes@ec.gc.ca>
2022-11-04 13:47:54 -0400

    Revert "Add NotFound methods. Change variable names"
    This reverts commit 5306394c667519b1c200476401863aa0b7c10ec6.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -26,15 +26,11 @@ public class CharacterController : ControllerBase
     }
 
     [HttpGet]
-    [Route("{characterId:int}")]
-    public async Task<ActionResult<CharacterDto>> GetCharacter(int characterId)
+    [Route("{id:int}")]
+    public async Task<ActionResult<CharacterDto>> GetCharacter(int id)
     {
-        var characterEntity = await _repository.GetCharacterAsync(characterId);
-        
-        if (characterEntity == null) return NotFound();
-
-        var characterMoves = await _repository.GetMovesForCharacterAsync(characterId); // idk how it knows to map onto the character entity; without this line, it is empty. It must be magic.
-        
+        var characterEntity = await _repository.GetCharacterAsync(id);
+        var characterMoves = await _repository.GetMovesForCharacterAsync(id); // idk how it knows to map onto the character entity; without this line, it is empty. It must be magic.
         return Ok(_mapper.Map<CharacterDto>(characterEntity));
     }
 
@@ -43,9 +39,6 @@ public class CharacterController : ControllerBase
     public async Task<ActionResult<IEnumerable<MoveDto>>> GetMoveListNoData(int characterId)
     {
         var characterMoves = await _repository.GetMovesForCharacterAsync(characterId);
-        
-        if (characterMoves == null) return NotFound();
-        
         return Ok(_mapper.Map<IEnumerable<MoveDto>>(characterMoves));
     }
 }
